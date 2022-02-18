e50a893fba41c38651de778e19c7c04582860c9a
Leon Wehrhan <mail@leonwehrhan.de>
2022-02-18 22:26:57 +0100

    test assign_sites

--- tests/test_track_s1_water.py
+++ tests/test_track_s1_water.py
@@ -3,28 +3,19 @@ import numpy as np
 from context import track_s1_water as tw
 
 
-@pytest.fixture
-def sample_result():
-    a = np.array([
-        [4081, 4085, 4093, 4097, 4109],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [4081, 4085, 4093, 4097, 4109],
-        [],
-        [],
-        []
-    ])
-
-    return a
-
-
 def test_voronoi():
-    assigned_sites = tw.assign_water_sites_voronoi(trj_file,
-                                                   ref_file,
-                                                   w_loc_ids,
-                                                   w_names,
+    assigned_sites = tw.assign_water_sites_voronoi('data/4y0z_a.pdb',
+                                                   'data/4y0z_t.pdb',
+                                                   [4078, 4081, 4072, 4084, 4075],
+                                                   ['A', 'B', 'C', 'D', 'E'],
                                                    ref_file_water=None,
                                                    verbose=False)
+
+    result = np.array([[4078, 0, 0, 0, 0],
+                       [0, 4081, 0, 0, 0],
+                       [0, 0, 4072, 0, 0],
+                       [0, 0, 0, 4084, 0],
+                       [0, 0, 0, 0, 4075],
+                       [4078, 4081, 4072, 4084, 4075],
+                       [0, 0, 0, 0, 0]])
+    assert (assigned_sites == result).all()
