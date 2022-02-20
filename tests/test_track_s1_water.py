b1f9a842334c30494b8a0014727ff2955dd2ba0b
Leon Wehrhan <mail@leonwehrhan.de>
2022-02-20 21:41:44 +0100

    test reduce_fluctuation

--- tests/test_track_s1_water.py
+++ tests/test_track_s1_water.py
@@ -19,3 +19,17 @@ def test_voronoi():
                        [4078, 4081, 4072, 4084, 4075],
                        [0, 0, 0, 0, 0]])
     assert (assigned_sites == result).all()
+
+
+def test_reduce_fluctuation():
+    assigned_sites = tw.assign_water_sites_voronoi('data/4y0z_fluc.pdb',
+                                                   'data/4y0z_t.pdb',
+                                                   [4078, 4081, 4072, 4084, 4075],
+                                                   ['A', 'B', 'C', 'D', 'E'],
+                                                   ref_file_water=None,
+                                                   verbose=False)
+
+    assigned_rf = tw.reduce_fluctuation(assigned_sites, 10)
+    count = tw.count_transitions(assigned_rf)
+
+    assert (count == np.array([1, 0, 0, 0, 0])).all()
